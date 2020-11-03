c9b365dc1dad28e904b4861aa6c2aa625fab5439
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2020-11-03 16:13:46 +0530

    climbing stairs+permutation Array recursion

--- Recursion.java
+++ Recursion.java
@@ -7,7 +7,44 @@ public class Recursion {
         You are climbing a stair case. It takes n steps to reach to the top.
         Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
     */
-    
+    public List<List<Integer>> permutation(int[] nums,int k,List<List<Integer>> result){
+
+
+        for(int i=k;i<nums.length;i++){
+
+            //swap
+
+            int temp = nums[i];
+            nums[i] = nums[k];
+            nums[k] = temp;
+
+            //permute
+
+            permutation(nums, k+1,result);
+
+            //swap
+
+            temp = nums[k];
+            nums[k] = nums[i];
+            nums[i] = temp;
+
+        }
+        if(k==nums.length-1){
+            List<Integer> arr = new ArrayList<Integer>();
+            for(int i=0;i<nums.length;i++){
+                    arr.add(nums[i]);
+            }
+            result.add(arr);
+        }
+        return result;
+
+    } 
+    public List<List<Integer>> permute(int[] nums){
+
+        return permutation(nums, 0,new ArrayList<List<Integer>>());
+
+    }
+
     public static int recursiveSol(int n){
         
         if(n<=1)return n;
