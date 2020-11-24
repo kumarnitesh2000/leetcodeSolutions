7db7c2a63530e1ffcf7d8254ff18d562081ef932
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2020-11-24 18:04:21 +0530

    maxDepthTree - Tree

---
+++ TreeNode.java
@@ -0,0 +1,39 @@
+public class TreeNode {
+    int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+       }
+}
+
+
+class Sol{
+
+
+    public static int maxDepth(TreeNode root) {
+        
+        if(root==null){
+            return 0;
+        }
+
+       int left = maxDepth(root.left);
+       int right = maxDepth(root.right);
+
+       return Math.max(left,right)+1; 
+        
+    }
+    public static void main(String[] args) {
+        //create a tree for our testCase
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);root.right = new TreeNode(29);
+        root.right.left = new TreeNode(15);root.right.right =  new TreeNode(7);
+        int res = maxDepth(root);
+        System.out.println(res);
+    }
+
+}
